53d2106c43db6e29fda246bdbacf27ecc770d91a
rasamkamyar <rasamkamyar@gmail.com>
2024-11-18 20:32:08 +0330

    Create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Navigate, Route, Routes } from "react-router-dom";
+import {
+  AboutPage,
+  CheckoutPage,
+  ContactPage,
+  DetailsPage,
+  NotFoundPage,
+  ProductsPage,
+} from "./components";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route index path="/" element={<Navigate to="/products" replace />} />
+      <Route path="/products" element={<ProductsPage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/contact" element={<ContactPage />} />
+      <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path="/products/:id" element={<DetailsPage />} />
+      <Route path="/*" element={<NotFoundPage />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import NotFoundPage from "./404";
+import AboutPage from "./AboutPage";
+import CheckoutPage from "./CheckoutPage";
+import ContactPage from "./ContactPage";
+import DetailsPage from "./DetailsPage";
+import ProductsPage from "./ProductsPage";
+
+export {
+  NotFoundPage,
+  DetailsPage,
+  CheckoutPage,
+  AboutPage,
+  ContactPage,
+  ProductsPage,
+};
